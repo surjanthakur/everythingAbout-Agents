f43f5427603d4f4b76eda3394b7361302871d555
surjanthakur <tsurjan506@gmail.com>
2025-10-06 23:06:45 +0530

    save

---
+++ frontend/src/data/resourceData.js
@@ -0,0 +1,7 @@
+const ResourceDate = [
+  {
+    title: "learn about prompts",
+    about: "im this section you learn about prompts",
+    url: "https://help.openai.com/en/articles/4936848-how-do-i-create-a-good-prompt-for-an-ai-model",
+  },
+];
