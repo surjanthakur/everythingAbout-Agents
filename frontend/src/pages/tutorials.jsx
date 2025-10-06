85aca75ff20078e373773347ad5ba814da68e835
surjanthakur <tsurjan506@gmail.com>
2025-10-06 23:27:17 +0530

    save

--- frontend/src/pages/tutorials.jsx
+++ frontend/src/pages/tutorials.jsx
@@ -1,9 +1,30 @@
-import Card from "../components/tutorialsCard";
+import TutorialData from "../data/tutorialsData";
+import { Link } from "react-router-dom";
+import "../css/tutorial.css";
 
 export default function Tutorial() {
   return (
     <>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 "></div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 ">
+        {TutorialData.map((data, indx) => (
+          <div className="Tcard ms-3" key={indx}>
+            <div className="w-full h-50 overflow-hidden ">
+              <img
+                className="w-full h-full object-cover"
+                src={data.image}
+                alt="image"
+              />
+            </div>
+
+            <span className="Tcard__title">{data.title}</span>
+            <p className="Tcard__content">{data.about}</p>
+
+            <Link to={data.url}>
+              <button className="Tcard__button">view playlist</button>
+            </Link>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
