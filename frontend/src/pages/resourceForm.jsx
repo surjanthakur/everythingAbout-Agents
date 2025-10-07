316fe8c323888e9ba0e512b45b7340e2ea2540f5
surjanthakur <tsurjan506@gmail.com>
2025-10-07 22:50:54 +0530

    save

---
+++ frontend/src/pages/resourceForm.jsx
@@ -0,0 +1,17 @@
+export default function CreateResource() {
+  return (
+    <>
+      <div>
+        <h1>contribute any content about agents , llm</h1>
+        <form>
+          <label>enter title</label>
+          <input></input>
+          <label>enter description</label>
+          <textarea></textarea>
+          <label>enter docs url</label>
+          <input></input>
+        </form>
+      </div>
+    </>
+  );
+}
---
+++ frontend/src/pages/tutorialForm.jsx
@@ -0,0 +1,17 @@
+export default function CreateTutorial() {
+  return (
+    <>
+      <div>
+        <h1>contribute any content about agents , llm</h1>
+        <form>
+          <label>enter title</label>
+          <input></input>
+          <label>enter description</label>
+          <textarea></textarea>
+          <label>enter video url</label>
+          <input></input>
+        </form>
+      </div>
+    </>
+  );
+}
