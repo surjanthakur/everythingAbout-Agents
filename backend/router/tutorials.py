3ffeb1eead205585acd480d3ddeae48af6affcbf
surjanthakur <tsurjan506@gmail.com>
2025-10-08 02:32:15 +0530

    save

--- backend/router/tutorials.py
+++ backend/router/tutorials.py
@@ -4,3 +4,38 @@ from databaseSchema import Tutorials
 from pydanticModel import ShowFields
 from database import get_session
 import uuid
+
+
+router = APIRouter(tags=["tutorial video routes"])
+
+
+@router.get("/allTutorials")
+def get_allTutorials(session_db: Session = Depends(get_session)):
+    try:
+        all_tutorials = session_db.query(Tutorials).all()
+        return all_tutorials
+    except Exception as err:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"tutorials not found {err}"
+        )
+
+
+@router.post("/tutorials/create")
+def create_resources(form_data: ShowFields, session_db: Session = Depends(get_session)):
+    try:
+        tutorial_id = str(uuid.uuid4())
+        new_tutorial = Tutorials(
+            id=tutorial_id,
+            title=form_data.title,
+            about=form_data.about,
+            url=form_data.url,
+        )
+        session_db.add(new_tutorial)
+        session_db.commit()
+        session_db.refresh(new_tutorial)
+        return new_tutorial
+    except Exception as err:
+        raise HTTPException(
+            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
+            detail=f"cant create resource: {err}",
+        )
