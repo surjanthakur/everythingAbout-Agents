d47e33a0051adfb65628aecf9ae1ad2d47b79991
surjanthakur <tsurjan506@gmail.com>
2025-10-08 03:29:42 +0530

    save

--- backend/main.py
+++ backend/main.py
@@ -16,7 +16,7 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
-origins = [" http://localhost:5173"]
+origins = ["http://localhost:5173"]
 
 app.add_middleware(
     CORSMiddleware,
