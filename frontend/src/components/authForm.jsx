fae8c7ab760d1eefc7ddfc7ae85d623a4114cd13
surjanthakur <tsurjan506@gmail.com>
2025-10-09 18:39:52 +0530

    save

--- frontend/src/components/authForm.jsx
+++ frontend/src/components/authForm.jsx
@@ -18,7 +18,7 @@ export default function AuthForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://127.0.0.1:8000/");
+      await axios.post("http://127.0.0.1:8000/signup");
     } catch (e) {
       console.log("err");
     }
