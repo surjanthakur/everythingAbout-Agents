711ba1100fa76fe971340b6e80d19f35d6d5a54d
surjanthakur <tsurjan506@gmail.com>
2025-10-09 23:56:43 +0530

    save

--- frontend/src/components/authForm.jsx
+++ frontend/src/components/authForm.jsx
@@ -6,7 +6,7 @@ import { Toaster, toast } from "react-hot-toast";
 
 export default function AuthForm() {
   const navigate = useNavigate();
-  const [inputData, setinputData] = useState({
+  const [signupData, setSignupData] = useState({
     username: "",
     email: "",
     password: "",
@@ -14,10 +14,10 @@ export default function AuthForm() {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setinputData((prev) => ({ ...prev, [name]: value }));
+    setSignupData((prev) => ({ ...prev, [name]: value }));
   };
 
-  const handleSubmit = async (e) => {
+  const handleSignupSubmit = async (e) => {
     e.preventDefault();
     try {
       const res = await axios.post("http://127.0.0.1:8000/signup", inputData);
@@ -50,12 +50,16 @@ export default function AuthForm() {
                     name="email"
                     placeholder="enter email"
                     type="email"
+                    value={""}
+                    onChange={handleChange}
                   />
                   <input
                     className="flip-card__input"
                     name="password"
                     placeholder=" enter Password"
                     type="password"
+                    value={""}
+                    onChange={handleChange}
                   />
                   <button type="submit" className="flip-card__btn">
                     Let&apos;s go!
@@ -66,13 +70,13 @@ export default function AuthForm() {
               {/* signup form */}
               <div className="flip-card__back">
                 <div className="title">Sign up</div>
-                <form onSubmit={handleSubmit} className="flip-card__form">
+                <form onSubmit={handleSignupSubmit} className="flip-card__form">
                   <input
                     className="flip-card__input"
                     placeholder="enter username"
                     name="username"
                     type="text"
-                    value={inputData.username}
+                    value={signupData.username}
                     onChange={handleChange}
                   />
                   <input
@@ -80,7 +84,7 @@ export default function AuthForm() {
                     name="email"
                     placeholder=" enter Email"
                     type="email"
-                    value={inputData.email}
+                    value={signupData.email}
                     onChange={handleChange}
                   />
                   <input
@@ -88,7 +92,7 @@ export default function AuthForm() {
                     name="password"
                     placeholder="enter Password"
                     type="password"
-                    value={inputData.password}
+                    value={signupData.password}
                     onChange={handleChange}
                   />
                   <button type="submit" className="flip-card__btn">
