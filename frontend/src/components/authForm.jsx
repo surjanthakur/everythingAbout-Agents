d55157c5e62f47727255ea0f13ad4f957b9f2d73
surjanthakur <tsurjan506@gmail.com>
2025-10-09 17:05:53 +0530

    save

--- frontend/src/components/authForm.jsx
+++ frontend/src/components/authForm.jsx
@@ -1,6 +1,22 @@
 import "./authform.css";
+import { useState, useEffect } from "react";
 
 export default function AuthForm() {
+  const [inputData, setinputData] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setinputData((prev) => ({ ...prev, [name]: value }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="form-wrappe wrapper">
       <div className="card-switch">
@@ -41,18 +57,24 @@ export default function AuthForm() {
                   placeholder="enter username"
                   name="username"
                   type="text"
+                  value={inputData.username}
+                  onChange={handleChange}
                 />
                 <input
                   className="flip-card__input"
                   name="email"
                   placeholder=" enter Email"
                   type="email"
+                  value={inputData.email}
+                  onChange={handleChange}
                 />
                 <input
                   className="flip-card__input"
                   name="password"
                   placeholder="enter Password"
                   type="password"
+                  value={inputData.password}
+                  onChange={handleChange}
                 />
                 <button type="submit" className="flip-card__btn">
                   Confirm!
